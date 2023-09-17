a67239f9fb89dd5fa3aaa2a92822432e2674288c
Mike Owino <mike.owino40@yahoo.com>
2023-09-17 20:57:57 +0000

    explorer

--- app/currencies/[asset]/page.tsx
+++ app/currencies/[asset]/page.tsx
@@ -3,6 +3,8 @@
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import Loading from "@/app/components/loading";
+import Link from "next/link";
+import Image from "next/image";
 
 interface Asset {
   id: number;
@@ -44,37 +46,118 @@ export default function AssetPage() {
   }
   // Render the track details
   return (
-    <div className="flex justify-center	items-center h-screen bg-white dark:bg-stone-900 dark:text-white">
-      {asset && (
-        <div>
-          <h1>{asset.name}</h1>
-          <p>{asset.rank}</p>
-          <p>{asset.symbol}</p>
-          <p>${asset.priceUsd}</p>
+    <>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            {asset && (
+              <>
+                <h1 className="text-5xl font-bold">{asset.name}</h1>
+                <p className="py-6">
+                  Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                  assumenda excepturi exercitationem quasi. In deleniti eaque
+                  aut repudiandae et a id nisi.
+                </p>
+                <Link href="/">
+                  <button className="btn btn-primary">Currencies</button>
+                </Link>
+              </>
+            )}
+          </div>
+          <div className="stats shadow">
+            <div className="stat">
+              <div className="stat-figure text-secondary">
+                <div className="avatar online">
+                  <div className="w-16 rounded-full">
+                    <Image
+                      src={`https://cryptologos.cc/logos/${
+                        asset.id
+                      }-${asset.symbol.toLowerCase()}-logo.png`}
+                      alt={asset.symbol}
+                      height={500}
+                      width={500}
+                      className="w-20 h-20 bg-zinc-300 rounded-lg dark:bg-zinc-600"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="stat-value">
+                {!isNaN(parseFloat(asset.changePercent24Hr))
+                  ? `${parseFloat(asset.changePercent24Hr).toFixed(2)}%`
+                  : "N/A"}
+              </div>
+              <div className="stat-title">24Hr Change</div>
+              {/* <div className="stat-desc text-secondary">31 tasks remaining</div> */}
+            </div>
+            <div className="stat">
+              <div className="stat-figure text-primary">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="inline-block w-8 h-8 stroke-current"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                  ></path>
+                </svg>
+              </div>
+              <div className="stat-title">Current Price</div>
+              <div className="stat-value text-primary">
+                {!isNaN(parseFloat(asset.priceUsd))
+                  ? `$${parseFloat(asset.priceUsd).toFixed(2)}`
+                  : "N/A"}
+              </div>
+              {/* <div className="stat-desc">21% more than last month</div> */}
+            </div>
+
+            <div className="stat">
+              {/* <div className="stat-figure text-secondary">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="inline-block w-8 h-8 stroke-current"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M13 10V3L4 14h7v7l9-11h-7z"
+                  ></path>
+                </svg>
+              </div> */}
+              <div className="stat-title">Page Views</div>
+              <a href={asset.explorer} target="_blank">
+                <button className="btn btn-outline btn-accent mt-2">
+                  {" "}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
+                    />
+                  </svg>
+                  Explorer
+                </button>
+              </a>
+
+              {/* <div className="stat-value text-secondary">2.6M</div> */}
+              {/* <div className="stat-desc">21% more than last month</div> */}
+            </div>
+          </div>
         </div>
-      )}
-    </div>
-    // <>
-    //   <section className="bg-white dark:bg-gray-900 bg-gradient-to-r from-violet-500 to-fuchsia">
-    //     <div className="container flex flex-col px-6 py-4 mx-auto space-y-6 h-max">
-    //       <section className="bg-white dark:bg-gray-900 xl:h-screen lg:h-screen bg-gradient-to-r from-violet-500 to-fuchsia">
-    //         <div className="container flex flex-col items-center px-4 py-12 mx-auto xl:flex-row">
-    //           <div className="flex justify-center xl:w-1/3 sm:pt-20 xl:pt-32 md:pt-10"></div>
-    //           <div className="flex flex-col items-center mt-6 xl:items-start xl:w-1/2 xl:mt-0">
-    //             <h2 className="text-2xl font-semibold tracking-tight text-gray-800 xl:text-3xl dark:text-white pb-2.5">
-    //               {asset.rank}
-    //             </h2>
-    //             <h2 className="text-1xl font-bold tracking-tight text-gray-800 xl:text-1xl dark:text-white pb-4">
-    //               Asset : {asset.name}
-    //             </h2>
-    //             <h2 className="text-1xl font-bold tracking-tight text-gray-800 xl:text-1xl dark:text-white pb-8">
-    //               Genre : {asset.id}
-    //             </h2>
-    //           </div>
-    //         </div>
-    //       </section>
-    //     </div>
-    //   </section>
-    // </>
+      </div>
+    </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -39,34 +39,69 @@ export default function Home() {
 
   return (
     <>
-      {/* <Header /> */}
-      <section className="bg-white dark:bg-zinc-900">
-        <div className="container flex flex-col px-6 py-4 mx-auto space-y-6 h-max">
-          <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 2xl:grid-cols-12 lg:grid-cols-7 md:grid-cols-3 sm:grid-cols-2">
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                <label></label>
+              </th>
+              <th>Currency</th>
+              <th>Price</th>
+              <th>24 Hrs Change %</th>
+              <th>Supply</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
             {assets.map((asset) => (
-              <div className="w-full" key={asset.id}>
-                <Link href={`/currencies/${asset.id}`}>
-                  <Image
-                    src={`https://cryptologos.cc/logos/${
-                      asset.id
-                    }-${asset.symbol.toLowerCase()}-logo.png`}
-                    alt={asset.symbol}
-                    height={500}
-                    width={500}
-                    className="w-20 h-20 bg-zinc-300 rounded-lg dark:bg-zinc-600"
-                  />
-                  {/* <h1 className="text-2xl font-semibold text-zinc-800 dark:text-white lg:text-1xl mb-2.5">
-                    {asset.name}
-                  </h1> */}
-                  <p className="text-1xl dark:text-white font-bold">
-                    {asset.symbol}
-                  </p>
-                </Link>
-              </div>
+              <tr key={asset.id}>
+                <th>
+                  <label>{asset.rank}</label>
+                </th>
+                <td>
+                  <Link href={`/currencies/${asset.id}`}>
+                    <div className="flex items-center space-x-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle w-12 h-12">
+                          <Image
+                            src={`https://cryptologos.cc/logos/${
+                              asset.id
+                            }-${asset.symbol.toLowerCase()}-logo.png`}
+                            alt={asset.symbol}
+                            height={500}
+                            width={500}
+                            className="w-20 h-20 bg-zinc-300 rounded-lg dark:bg-zinc-600"
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-bold">{asset.name}</div>
+                        <div className="text-sm opacity-50">{asset.symbol}</div>
+                      </div>
+                    </div>
+                  </Link>
+                </td>
+                <td>
+                  $ {asset.priceUsd}
+                  {/* <br />
+                  <span className="badge badge-ghost badge-sm">
+                    Desktop Support Technician
+                  </span> */}
+                </td>
+                <td>{asset.changePercent24Hr}</td>
+                <th>
+                  <button className="btn btn-ghost btn-xs">
+                    {asset.supply}
+                  </button>
+                </th>
+              </tr>
             ))}
-          </div>
-        </div>
-      </section>
+          </tbody>
+          {/* foot */}
+        </table>
+      </div>
     </>
   );
 }
