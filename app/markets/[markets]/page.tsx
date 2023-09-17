f309a064e4f8b0218a15ca1b88a4d36e1290b789
Mike Owino <mike.owino40@yahoo.com>
2023-09-17 18:48:29 +0000

    cryptologos

---
+++ app/markets/[markets]/page.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import Loading from "@/app/components/loading";
+
+interface Asset {
+  exchangeId: string;
+  baseId: string;
+  quoteId: string;
+  baseSymbol: string;
+  quoteSymbol: string;
+  volumeUsd24Hr: number;
+  priceUsd: number;
+  volumePercent: Number;
+}
+
+export default function MarketsPage() {
+  //   const audioRef = useRef<HTMLAudioElement | null>(null); // Declare audioRef
+
+  const params = useParams();
+  // const id = params.get("id") as string;
+  // console.log(params.track);
+  const id = params.asset;
+
+  const [asset, setAsset] = useState<Asset | null>(null);
+
+  useEffect(() => {
+    const fetchAsset = async () => {
+      const response = await fetch(
+        `https://api.coincap.io/v2/assets/${id}/markets`
+      );
+      const responseData = await response.json();
+      setAsset(responseData.data);
+    };
+
+    fetchAsset();
+  }, [id]);
+
+  if (!asset) {
+    return <Loading />;
+  }
+  // Render the track details
+  return (
+    <div className="flex justify-center	items-center h-screen bg-white dark:bg-stone-900 dark:text-white">
+      {asset && (
+        <div>
+          <h1>{asset.exchangeId}</h1>
+          <p>{asset.quoteId}</p>
+          <p>{asset.baseId}</p>
+          <p>${asset.priceUsd}</p>
+        </div>
+      )}
+    </div>
+    // <>
+    //   <section className="bg-white dark:bg-gray-900 bg-gradient-to-r from-violet-500 to-fuchsia">
+    //     <div className="container flex flex-col px-6 py-4 mx-auto space-y-6 h-max">
+    //       <section className="bg-white dark:bg-gray-900 xl:h-screen lg:h-screen bg-gradient-to-r from-violet-500 to-fuchsia">
+    //         <div className="container flex flex-col items-center px-4 py-12 mx-auto xl:flex-row">
+    //           <div className="flex justify-center xl:w-1/3 sm:pt-20 xl:pt-32 md:pt-10"></div>
+    //           <div className="flex flex-col items-center mt-6 xl:items-start xl:w-1/2 xl:mt-0">
+    //             <h2 className="text-2xl font-semibold tracking-tight text-gray-800 xl:text-3xl dark:text-white pb-2.5">
+    //               {asset.rank}
+    //             </h2>
+    //             <h2 className="text-1xl font-bold tracking-tight text-gray-800 xl:text-1xl dark:text-white pb-4">
+    //               Asset : {asset.name}
+    //             </h2>
+    //             <h2 className="text-1xl font-bold tracking-tight text-gray-800 xl:text-1xl dark:text-white pb-8">
+    //               Genre : {asset.id}
+    //             </h2>
+    //           </div>
+    //         </div>
+    //       </section>
+    //     </div>
+    //   </section>
+    // </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ import { number } from "prop-types";
 import Loading from "@/app/components/loading";
 
 interface Asset {
-  id: number;
-  rank: string;
+  id: string;
+  rank: number;
   symbol: string;
   name: string;
   supply: number;
@@ -39,21 +39,23 @@ export default function Home() {
   return (
     <>
       {/* <Header /> */}
-      <section className="bg-white dark:bg-stone-900">
+      <section className="bg-white dark:bg-zinc-900">
         <div className="container flex flex-col px-6 py-4 mx-auto space-y-6 h-max">
           <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 2xl:grid-cols-12 lg:grid-cols-7 md:grid-cols-3 sm:grid-cols-2">
             {assets.map((asset) => (
               <div className="w-full" key={asset.id}>
-                <Link href={`/assets/${asset.id}`}>
+                <Link href={`/currencies/${asset.id}`}>
                   <img
-                    src="https://logosave.com/images/large/common/02/bitcoin.png"
-                    className="w-20 h-20 bg-stone-300 rounded-lg dark:bg-stone-600"
+                    src={`https://cryptologos.cc/logos/${
+                      asset.id
+                    }-${asset.symbol.toLowerCase()}-logo.png`}
+                    className="w-20 h-20 bg-zinc-300 rounded-lg dark:bg-zinc-600"
                   />
-                  {/* <h1 className="text-2xl font-semibold text-stone-800 dark:text-white lg:text-1xl mb-2.5">
+                  {/* <h1 className="text-2xl font-semibold text-zinc-800 dark:text-white lg:text-1xl mb-2.5">
                     {asset.name}
                   </h1> */}
                   <p className="text-1xl dark:text-white font-bold">
-                    {asset.name}
+                    {asset.symbol}
                   </p>
                 </Link>
               </div>
