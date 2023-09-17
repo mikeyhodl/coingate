5a9dea6754bfc51c455eb57bca69d688d817ccc2
Mike Owino <mike.owino40@yahoo.com>
2023-09-17 21:19:58 +0000

    https://daisyui.com/images/stock/p.jpg

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -20,7 +20,7 @@ export default function Test() {
           <div className="dropdown dropdown-end">
             <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
               <div className="w-10 rounded-full">
-                <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
               </div>
             </label>
             <ul
