0f9e33a588c741ca908453ce067eb6e82229bd05
Mike Owino <mike.owino40@yahoo.com>
2023-09-19 06:10:21 +0000

    yellow-100

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,35 +1,32 @@
 import Link from "next/link";
+import Hotimage from "./hot.svg";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <>
       <div className="navbar bg-base-100 sticky top-0 z-30">
-        {/* <div className="flex-1">
-          <Link href="/" className="btn btn-glass normal-case text-xl">
-            CoinGate
-          </Link>
-        </div> */}
         <div className="navbar-end hidden lg:flex">
-          {/* <ul className="menu menu-horizontal px-1"> */}
-          {/* <li> */}
           <Link href="/" className="btn mr-2">
             Coins
           </Link>
           <Link href="/exchanges" className="btn mr-2">
             Exchanges
           </Link>
-          {/* </li> */}
-          {/* <li> */}
+
           <Link href="/markets" className="btn mr-2">
             Markets
           </Link>
-          {/* </li> */}
-          {/* <li> */}
+
           <Link href="/" className="btn mr-2">
             History
           </Link>
-          {/* </li> */}
-          {/* </ul> */}
+          <Link href="/hotlist" className="btn mr-2">
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <span>CoinWatch</span>
+              <Image src={Hotimage} alt="Logo" height={24} width={24} />
+            </div>
+          </Link>
         </div>
         <div className="flex-none gap-2">
           <div className="form-control">
---
+++ app/hotlist/page.tsx
@@ -0,0 +1,19 @@
+export default function HotList() {
+  return (
+    <>
+      <div className="h-screen">
+        <section className="m-6">
+          <div className="card w-80 bg-yellow-100 text-primary-content">
+            <div className="card-body">
+              <h2 className="card-title">Card title!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy</button>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+}
